5a05ad297fc4e2e2bdb5ea75e85bad090b852d11
biinahc <sabrinacaldasads@gmail.com>
2026-02-02 19:28:27 -0400

    subindo

---
+++ src/controllers/sensor.controller.ts
@@ -0,0 +1,26 @@
+import type { Request, Response } from "express";
+import { SensorService } from "../services/SensorService.js";
+
+const service = new SensorService();
+
+export class SensorController {
+  async create(req: Request, res: Response) {
+    try {
+      const result = await service.create(req.body);
+      res.status(201).json(result);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const result = await service.update(Number(req.params.id), req.body);
+      res.json(result);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+}
+
+export const sensorController = new SensorController();
